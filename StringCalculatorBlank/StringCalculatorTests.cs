d7c52372a59fbadebaed65c1d60a95d9d2839b14
JeffryGonzalez <jeff@hypertheory.com>
2021-07-28 14:33:50 -0400

    Add project files.

---
+++ StringCalculatorBlank/StringCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+// Instructions: https://osherove.com/tdd-kata-1
+namespace StringCalculatorBlank
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            var calculator = new StringCalculator();
+            var numbers = "";
+
+            int sum = calculator.Add(numbers);
+
+            Assert.Equal(0, sum);
+        }
+    }
+}
